599aa854b44ba9a88ffe5a90658890b1f8443631
yuliuftd <liuyuftd@gmail.com>
2022-03-20 23:05:38 +1100

    use heroku-postbuild

--- server.js
+++ server.js
@@ -37,4 +37,13 @@ app.get("/yesMarketValue", async (req, res) => {
   res.send(total.toFixed(2).toString());
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT} ...`));
